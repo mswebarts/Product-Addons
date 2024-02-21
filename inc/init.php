91d84504e3ac3bbfe33ae551ad0d7c88761dedda
MS Shohan <mswebarts@gmail.com>
2024-02-21 07:05:19 +0600

    removed unnecessary codes and files

--- inc/init.php
+++ inc/init.php
@@ -4,15 +4,9 @@ global $mspa_dir;
 // including options panel
 include_once $mspa_dir . 'inc/admin/options-panel/options-panel.php';
 
-// include plugin files
-// include_once $mspa_dir . 'inc/functions/hooks-actions.php';
-
 // check if enable product addon is enabled
 $mspa_options = get_option('mspa_general_options');
 if (!empty($mspa_options['mspa_enable_product_addon'])) {
     include_once $mspa_dir . 'inc/functions/product-settings.php';
     include_once $mspa_dir . 'inc/functions/product-frontend.php';
 }
-
-// including template loader
-include_once $mspa_dir . 'inc/classes/class-mspa-template-loader.php';
