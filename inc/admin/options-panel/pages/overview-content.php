fc4d3bdff6cf28478456aa087127638e7b434aaf
MS Shohan <mswebarts@gmail.com>
2024-04-04 16:14:40 +0600

    msbr-column-content to mswa-column-content

--- inc/admin/options-panel/pages/overview-content.php
+++ inc/admin/options-panel/pages/overview-content.php
@@ -5,7 +5,7 @@
 	<div class="mswa-box">
 		<h3 class="mswa-column-title"><i class="dashicons-before dashicons-admin-links"></i> <?php esc_html_e( 'Important Links', 'msp-addons' ); ?> </h3>
 		<hr>
-		<div class="msbr-column-content">
+		<div class="mswa-column-content">
 			<ul>
 				<li><a href="https://wordpress.org/plugins/simple-product-addons/" target="_blank"><?php esc_html_e( 'Plugin Homepage', 'msp-addons' ); ?></a></li>
 				<li><a href="https://wordpress.org/support/plugin/simple-product-addons/" target="_blank"><?php esc_html_e( 'Support', 'msp-addons' ); ?></a></li>
