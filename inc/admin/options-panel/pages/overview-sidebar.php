00e6ad0c89f084eba01c8bcf2f4376c96605f360
MS Shohan <mswebarts@gmail.com>
2024-04-05 12:37:56 +0600

    updated review link from overview page

--- inc/admin/options-panel/pages/overview-sidebar.php
+++ inc/admin/options-panel/pages/overview-sidebar.php
@@ -7,7 +7,7 @@
 				<img src="<?php echo esc_url( $mspa_url . 'inc/admin/assets/images/rating.jpg' ); ?>" alt="<?php echo esc_attr_e( '5 star rating', 'simple-product-addons' ); ?>">
 				<div>
 					<?php echo esc_html_e( 'Leave us a 5 star review on WordPress and help spread the word!', 'simple-product-addons' ); ?>
-					<a href="<?php echo esc_url( 'https://wordpress.org/support/plugin/product-addons/reviews/#new-post' ); ?>" class="mspa-add-wordpress-review" target="_blank">
+					<a href="<?php echo esc_url( 'https://wordpress.org/support/plugin/simple-product-addons/reviews/#new-post' ); ?>" class="mspa-add-wordpress-review" target="_blank">
 						<?php echo esc_html_e( 'Review Us on', 'simple-product-addons' ) . ' '; ?>
 						<img src="<?php echo esc_url( $mspa_url . 'inc/admin/assets/images/wordpress-logo.png' ); ?>" alt="<?php echo esc_attr_e( 'WordPress Logo', 'simple-product-addons' ); ?>" style="margin-left: 5px;"/>
 					</a>
